91f9a5bd400c32d6815f2aa08d9f3b62a1f2e02c
elon <elon36k@mail.com>
2025-04-01 11:28:47 +0800

    update

--- build.js
+++ build.js
@@ -2,21 +2,52 @@ const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const archiver = require('archiver');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // Webpack配置
 const webpackConfig = {
   mode: 'production',
   entry: {
     content: './content.js',
-    popup: './popup.js'
+    popup: './popup.js',
+    styles: './styles.css'
   },
   output: {
     filename: '[name].min.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
   plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin()
-  ]
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
+  }
 };
 
 // 创建dist目录
@@ -37,7 +68,7 @@ webpack(webpackConfig, (err, stats) => {
   }));
   
   // 复制其他必要文件到dist目录
-  const filesToCopy = ['manifest.json', 'popup.html', 'styles.css', 'images'];
+  const filesToCopy = ['manifest.json', 'popup.html', 'images'];
   filesToCopy.forEach(file => {
     const srcPath = path.join(__dirname, file);
     const destPath = path.join(__dirname, 'dist', file);
@@ -46,26 +77,37 @@ webpack(webpackConfig, (err, stats) => {
       if (fs.lstatSync(srcPath).isDirectory()) {
         copyFolderSync(srcPath, destPath);
       } else {
-        fs.copyFileSync(srcPath, destPath);
+        if (file === 'popup.html') {
+          let htmlContent = fs.readFileSync(srcPath, 'utf8');
+          htmlContent = htmlContent.replace('popup.js', 'popup.min.js');
+          htmlContent = htmlContent.replace('styles.css', 'styles.min.css');
+          fs.writeFileSync(destPath, htmlContent);
+        } else if (file === 'manifest.json') {
+          let manifestContent = fs.readFileSync(srcPath, 'utf8');
+          manifestContent = manifestContent.replace('content.js', 'content.min.js');
+          fs.writeFileSync(destPath, manifestContent);
+        } else {
+          fs.copyFileSync(srcPath, destPath);
+        }
       }
     }
   });
   
-  // 创建zip压缩包
-  const output = fs.createWriteStream('twitter-thread-exporter.zip');
-  const archive = archiver('zip', { zlib: { level: 9 } });
+//   // 创建zip压缩包
+//   const output = fs.createWriteStream('twitter-thread-exporter.zip');
+//   const archive = archiver('zip', { zlib: { level: 9 } });
   
-  output.on('close', () => {
-    console.log(`压缩包创建完成，大小: ${archive.pointer()} bytes`);
-  });
+//   output.on('close', () => {
+//     console.log(`压缩包创建完成，大小: ${archive.pointer()} bytes`);
+//   });
   
-  archive.on('error', (err) => {
-    throw err;
-  });
+//   archive.on('error', (err) => {
+//     throw err;
+//   });
   
-  archive.pipe(output);
-  archive.directory('dist/', false);
-  archive.finalize();
+//   archive.pipe(output);
+//   archive.directory('dist/', false);
+//   archive.finalize();
 });
 
 // 递归复制文件夹
